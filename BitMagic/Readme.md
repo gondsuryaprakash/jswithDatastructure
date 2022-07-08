732a96fa5701c8e4f9ff9de0bbcd5a1500467c61
gondsuryaprakash <suryaprakashgond15243@gmail.com>
2022-07-08 08:26:33 +0530

    Bit Magic

--- BitMagic/Readme.md
+++ BitMagic/Readme.md
@@ -19,8 +19,18 @@ x = 33 , y = 2
 In Binarary : 0....000100001 // (32 bit)
 x>>y =  00......01000 = 8
 
-In MatheMatics: x>>y = floor(x/y^2)
+In MatheMatics: x>>y = floor(x/2^y)
 
 
+
+```
+
+### BitWise Not Operator ~ 
+
+Its revert the 0 to 1 and vice versa. 
+```
+ x = 00....00010 (32 Bits)
+ ~x = 11...11101 
+ In Mathmatics : For n bit of x number its equivalent to : 2^n -1 - x 
 
 ```
---
+++ BitMagic/kthNumberIsBit.js
@@ -0,0 +1,71 @@
+/*
+Problem : Find Kth bit is set or not 
+*/
+
+
+// Method 1 Left Shift: 
+
+function isKthBitIsSet(n,k) {
+    return n&(1<<(k-1)) != 0
+}
+
+/*
+Explaination Method 1: 
+Let n =5, k = 3 
+
+5 = 000101
+1<< 2 = 000100
+Now doing And 
+000101
+000100
+------
+000100 ----> Return true 
+
+let k =2
+1<<1 = 0000010 
+Now doing And 
+000101
+000010
+------
+000000  ---> 0 Return False 
+*/
+
+
+// Method 2 Right Shift 
+
+function isKthBitIsSetByRightShift(n,k) {
+    return n >> (k-1) & 1 === 1 
+}
+
+/*
+Explaination Method 2:
+Let n = 10 
+k = 3
+n = 0000...1010 (32 bit)
+n>>k-1 = 0000...10 (Add two 0 in right)
+
+Now doing And 
+0000....10
+0000....01
+-----------
+0000....00  ---> Return False
+
+Let k =4 
+n>> k -1 = 0000....01
+
+Now doing And
+0000....01
+0000....01
+----------
+0000....01 ---> 1 ---> return true
+
+
+
+
+
+
+
+
+
+
+*/
